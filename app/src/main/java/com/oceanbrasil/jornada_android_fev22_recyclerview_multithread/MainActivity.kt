be89ccd079337f42a08b79e0e443b98bf99929db
Paulo Salvatore <salvatoregames@gmail.com>
2022-02-08 12:12:19 -0300

    Exemplo de multithread

--- app/src/main/java/com/oceanbrasil/jornada_android_fev22_recyclerview_multithread/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/jornada_android_fev22_recyclerview_multithread/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.oceanbrasil.jornada_android_fev22_recyclerview_multithread
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,5 +34,39 @@ class MainActivity : AppCompatActivity() {
         val listaPokemons = listOf(charmander, pikachu)
 
         rvPokemons.adapter = PokemonsAdapter(listaPokemons)
+
+        // Exemplos de Multithread
+
+        val url = "QUALQUER URL DE IMAGEM"
+
+        // Estou na main thread
+
+        val ivImagem = findViewById<ImageView>(R.id.ivImagem)
+
+        Thread {
+            // Estou na worker thread
+
+            // Carregar a imagem (operação que pode levar um tempo)
+            val imagemBitmap = carregarImagem(url)
+
+            ivImagem.post {
+                // Estou de volta na main thread
+                ivImagem.setImageBitmap(imagemBitmap)
+            }
+        }.start()
+    }
+
+    private fun carregarImagem(urlImagem: String): Bitmap? {
+        try {
+            val url = URL(urlImagem)
+
+            val conteudoUrl = url.openConnection().getInputStream()
+
+            return BitmapFactory.decodeStream(conteudoUrl)
+        } catch (e: Exception) {
+            Log.e("CARREGAR_IMAGEM", "Algum erro ocorreu ao carregar a imagem.", e)
+
+            return null
+        }
     }
 }
